d5cad4f25150c2ba9abe0c0f50c94e681086ec49
1-blue <1-blue98@naver.com>
2025-12-09 12:48:18 +0900

    feat: 💄 [디자인시스템] 디자인시스템 페이지 구현

---
+++ src/app/(test)/design-system/_source/components/design-system-page.tsx
@@ -0,0 +1,7 @@
+import { Box } from '@chakra-ui/react/box'
+
+const DesignSystemPage: React.FC = () => {
+  return <Box p="40px" display="flex" flexDirection="column" gap="24px"></Box>
+}
+
+export default DesignSystemPage
