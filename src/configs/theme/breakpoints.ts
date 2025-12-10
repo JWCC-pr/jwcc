83d377df4aca70a797708da650e19573f3c3428f
1-blue <1-blue98@naver.com>
2025-12-10 10:04:28 +0900

    feat: 💄 [디자인시스템] breakpoint 설정

--- src/configs/theme/breakpoints.ts
+++ src/configs/theme/breakpoints.ts
@@ -1,7 +1,12 @@
+/**
+ * + `mobile`: `base` ~ `sm`
+ * + `tablet`: `sm` ~ `md`
+ * + `desktop`: `md` ~ `lg`
+ * + `lg desktop`: `lg` ~
+ */
 export const breakpoints = {
-  sm: '480px',
-  md: '768px',
-  lg: '1024px',
-  xl: '1280px',
-  '2xl': '1536px',
+  base: '0px',
+  sm: '768px',
+  md: '1280px',
+  lg: '1760px',
 }
