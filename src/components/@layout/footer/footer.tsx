331cf9b9be55d7cb0bcf84222aa67f744879da1d
1-blue <1-blue98@naver.com>
2025-12-12 11:03:18 +0900

    feat: 💄 [레이아웃] 푸터 퍼블리싱

---
+++ src/components/@layout/footer/footer.tsx
@@ -0,0 +1,136 @@
+import { Box } from '@chakra-ui/react/box'
+import { Text } from '@chakra-ui/react/text'
+
+import { LayoutFooterLogoIcon } from '@/generated/icons/MyIcons'
+
+const menuItems = [
+  {
+    label: '본당 소개',
+    subItems: [
+      { label: '본당 소개' },
+      { label: '본당 역사' },
+      { label: '주보 성인' },
+      { label: '본당 상징 및 로고' },
+      { label: '사목지침' },
+      { label: '본당 사제 · 수도자' },
+      { label: '역대 사제 · 수도자' },
+      { label: '미사시간 안내 및 오시는 길' },
+      { label: '본당 행사 일정' },
+    ],
+  },
+  {
+    label: '본당 소식',
+    subItems: [
+      { label: '공지사항' },
+      { label: '본당 행사' },
+      { label: '본당 주보' },
+      { label: '서울 주보' },
+      { label: '선종 안내' },
+      { label: '전례꽃 갤러리' },
+      { label: '자유게시판' },
+      { label: '자료실' },
+    ],
+  },
+  {
+    label: '신앙 공동체',
+    subItems: [
+      { label: '사목협의회 조직도' },
+      { label: '본당 관할 구역도' },
+      { label: '분과 바로가기' },
+    ],
+  },
+  {
+    label: '본당 업무',
+    subItems: [
+      { label: '사무실 안내' },
+      { label: '예비신자 안내' },
+      { label: '혼인성사 예약 안내' },
+      { label: '전입 교우 안내' },
+    ],
+  },
+]
+
+const Footer: React.FC = () => {
+  return (
+    <Box as="footer" bg="grey.1">
+      <Box
+        p="20px 40px"
+        display="flex"
+        gap="12px"
+        alignItems="center"
+        borderBottom="1px solid"
+        borderBottomColor="border.basic.1"
+      >
+        <Text textStyle="pre-body-5" color="grey.10">
+          개인정보처리방침
+        </Text>
+        <Text textStyle="pre-body-5" color="grey.7">
+          ·
+        </Text>
+        <Text textStyle="pre-body-5" color="grey.7">
+          이용약관
+        </Text>
+      </Box>
+      <Box p="24px 40px 36px" display="flex" gap="36px">
+        <Box flex="1" display="flex" gap="16px">
+          {menuItems.map((item) => (
+            <Box
+              key={item.label}
+              flex="1"
+              display="flex"
+              flexFlow="column nowrap"
+              gap="8px"
+            >
+              <Text textStyle="pre-body-5" color="grey.8">
+                {item.label}
+              </Text>
+
+              <Box display="flex" flexFlow="column nowrap" gap="6px">
+                {item.subItems.map((subItem) => (
+                  <Text
+                    key={subItem.label}
+                    textStyle="pre-body-6"
+                    color="grey.6"
+                  >
+                    {subItem.label}
+                  </Text>
+                ))}
+              </Box>
+            </Box>
+          ))}
+        </Box>
+        <Box w="1px" h="100%" />
+        <Box
+          w="300px"
+          display="flex"
+          flexFlow="column nowrap"
+          justifyContent="space-between"
+        >
+          <Box display="flex" flexFlow="column nowrap" gap="24px">
+            <Box display="flex" flexFlow="column nowrap" gap="8px">
+              <Text textStyle="pre-body-5" color="grey.8">
+                Contact
+              </Text>
+              <Box display="flex" flexFlow="column nowrap" gap="4px">
+                <Text textStyle="pre-body-6" color="grey.6">
+                  서울 종로구 창신동 236-2 잠원동 성당
+                </Text>
+                <Text textStyle="pre-body-6" color="grey.6">
+                  02-742-3311
+                </Text>
+              </Box>
+            </Box>
+            <Text textStyle="pre-caption-2" color="grey.5">
+              Copyright © 잠원동 성당. ALLRights Reserved.
+            </Text>
+          </Box>
+          <Box alignSelf="flex-end">
+            <LayoutFooterLogoIcon w="124px" h="37px" />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+export default Footer
