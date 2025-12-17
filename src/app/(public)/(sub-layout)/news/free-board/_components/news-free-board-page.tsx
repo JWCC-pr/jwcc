ac68854795ca9ef642a804c1778cb34c762b176f
1-blue <1-blue98@naver.com>
2025-12-17 15:38:19 +0900

    chore: 🚀 빌드 이슈 처리
    useSearchParams()는 <Suspense> 내부에 사용

--- src/app/(public)/(sub-layout)/news/free-board/_components/news-free-board-page.tsx
+++ src/app/(public)/(sub-layout)/news/free-board/_components/news-free-board-page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react'
+
 import { Box } from '@chakra-ui/react/box'
 
 import NewFreeBoardTableSection from './new-free-board-table-section'
@@ -5,11 +7,13 @@ import NewFreeBoardTopSection from './sections/new-free-board-top-section'
 
 const NewsFreeBoardPage: React.FC = () => {
   return (
-    <Box display="flex" flexDirection="column">
-      <NewFreeBoardTopSection />
+    <Suspense>
+      <Box display="flex" flexDirection="column">
+        <NewFreeBoardTopSection />
 
-      <NewFreeBoardTableSection />
-    </Box>
+        <NewFreeBoardTableSection />
+      </Box>
+    </Suspense>
   )
 }
 
