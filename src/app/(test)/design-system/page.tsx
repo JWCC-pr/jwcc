49138705203ff32e179dc130938684711e3eb1f8
1-blue <1-blue98@naver.com>
2025-12-30 12:23:33 +0900

    chore: ♻️ 미사용 코드 제거

--- src/app/(test)/design-system/page.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next'
-
-import DesignSystemPage from './_source/components/design-system-page'
-
-const Page: NextPage = () => {
-  return <DesignSystemPage />
-}
-
-export default Page
