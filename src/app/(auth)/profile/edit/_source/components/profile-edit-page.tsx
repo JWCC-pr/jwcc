d11df546bd82206ff922412334c70965d36a882f
1-blue <1-blue98@naver.com>
2026-01-09 10:25:52 +0900

    refactor: ♻️ 프로필 수정 및 비밀번호 초기화 이슈 처리

--- src/app/(auth)/profile/edit/_source/components/profile-edit-page.tsx
+++ src/app/(auth)/profile/edit/_source/components/profile-edit-page.tsx
@@ -1,9 +1,28 @@
 'use client'
 
+import useMe from '@/hooks/useMe'
+
 import ProfileEditForm from './form/profile-edit-form'
 
 const ProfileEditPage: React.FC = () => {
-  return <ProfileEditForm />
+  const { data: me } = useMe()
+
+  if (!me) return null
+
+  const [year, month, day] = me.birth.split('-').map((v) => String(Number(v)))
+
+  return (
+    <ProfileEditForm
+      defaultValues={{
+        name: me.name,
+        baptismName: me.baptismalName,
+        address: me.baseAddress,
+        addressDetail: me.detailAddress,
+        postcode: me.postcode,
+        birthDate: { year, month, day },
+      }}
+    />
+  )
 }
 
 export default ProfileEditPage
